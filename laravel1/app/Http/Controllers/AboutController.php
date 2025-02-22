25b38e7048afc688114de310a1bc3ad4751cfcab
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-02-22 21:12:37 +0700

    jobsheet 2:  controller

---
+++ laravel1/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Nama: Annisa Eka Puspita, NIM: 2341720131';
+    }
+}
