580386eeb6b2a198ae29fc7c9fc0f7c8b3cc5bb9
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-03-05 00:14:45 +0700

    Jobsheet 2: Routing

--- Minggu2/PWL_2025/routes/web.php
+++ Minggu2/PWL_2025/routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/hello', function () {
+//    return 'Hello World';
+//});
+
+//Route::get('/world', function () {
+//    return 'World';
+//});
+
+//Route::get('/', function () {
+//    return 'Selamat Datang';
+//});
+
+//Route::get('/about', function () {
+//    return '2341720131, Annisa Eka Puspita';
+//});
+
+//Route::get('/user/{name}', function ($nama) {
+//    return 'Nama saya '.$nama;
+//});
+
+//Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) { 
+//    return 'Pos ke-'.$postId.", Komentar ke-".$commentId; 
+//});
+
+//Route::get('/articles/{id}', function ($id) { 
+//    return 'Halaman Artikel dengan ID '.$id;
+//}); 
+
+//Route::get('/user/{name?}', function ($name=null) { 
+//    return 'Nama saya '.$name; 
+//});
+
+Route::get('/user/{name?}', function ($name='John') { 
+    return 'Nama saya '.$name; 
 });
