cf61954a1a0ba67ceccad1471c28c01edaed20a7
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-03-12 09:41:21 +0700

    Jobsheet 4

---
+++ Minggu4/PWL_POS/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - POS</title>
+</head>
+<body>
+    <h1>Welcome to POS (Point of Sales)</h1>
+    <h3>Menu:</h3>
+    <a href="/category"><button>Products</button></a>
+    <br> <br>
+    <a href="/user/2341720131/name/Annisa Eka Puspita"><button>User</button></a>
+    <br> <br>
+    <a href="/sales"><button>Sales</button></a>
+</body>
+</html>
+
+
+
