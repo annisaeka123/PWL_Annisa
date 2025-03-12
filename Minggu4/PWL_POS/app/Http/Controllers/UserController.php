f1af1bcd7ec67fa2c750616763741b8db5d4afc1
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-03-12 16:50:59 +0700

    Jobsheet 4: Praktikum 1 ($fillable)

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,15 +10,37 @@ class UserController extends Controller
 {
     public function index()
     {
-        //tambah data dengan Eloquent Model
+
+        /**
+         * Jobsheet 4, praktikum 1
+         */
+
+        //Untuk menambahkan data baru
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga', 
+            //'username' => 'manager_dua', 
+            'nama' => 'Manager 3',
+            //'nama' => 'Manager 2',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        
+        UserModel::create($data);
 
         //mencoba mengakses UserModel
         $user = UserModel::all(); //ambil semua data dari tabel m_user
         return view('user', ['data' => $user]);
+        
+        /**
+         * Jobsheet 2
+         */
+        
+        //tambah data dengan Eloquent Model
+        //$data = [
+        //    'nama' => 'Pelanggan Pertama',
+        //];
+        //UserModel::where('username', 'customer-1')->update($data); //update data user
+
     }
 }
 
