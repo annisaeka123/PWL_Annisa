e3a25ad32d5088f70b09fafe8d9c01d0e1f684d7
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-03-12 17:34:57 +0700

    Jobsheet 4: Praktikum 2.1 (Retrieving Single Models)

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,30 +9,55 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-    {
-
+    {        
         /**
-         * Jobsheet 4, praktikum 1
+         * Jobsheet 4
          */
+        //PRAKTIKUM 2.1 - LANGKAH 1
+        //$user = UserModel::find(1);
+        //return view('user', ['data' => $user]);
 
-        //Untuk menambahkan data baru
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga', 
-            //'username' => 'manager_dua', 
-            'nama' => 'Manager 3',
-            //'nama' => 'Manager 2',
-            'password' => Hash::make('12345')
-        ];
+        //PRAKTIKUM 2.1 - LANGKAH 4
+        //$user = UserModel::firstWhere('level_id', 1)->first();
+        //return view('user', ['data' => $user]);
+
+        //PRAKTIKUM 2.1 - LANGKAH 6
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);       
         
-        UserModel::create($data);
+        //PRAKTIKUM 2.1 - LANGKAH 8
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+            // abort(404);
+        // });
+        //return view('user', ['data' => $user]);
+
+        //PARKTIKUM 2.1 - LANGKAH 10
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        
+        return view('user', ['data' => $user]);
+
+        //***************************************************************************************** */
+        //PRAKTIKUM 1
+        //$data = [  //Untuk menambahkan data baru
+        //    'level_id' => 2,
+        //    'username' => 'manager_tiga', 
+            //'username' => 'manager_dua', 
+        //    'nama' => 'Manager 3',
+            //'nama' => 'Manager 2',
+        //    'password' => Hash::make('12345')
+        //];
+        
+        //UserModel::create($data);
 
         //mencoba mengakses UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        //$user = UserModel::all(); //ambil semua data dari tabel m_user
+        //return view('user', ['data' => $user]);
         
+        //=======================================================================================
         /**
-         * Jobsheet 2
+         * Jobsheet 3
          */
         
         //tambah data dengan Eloquent Model
