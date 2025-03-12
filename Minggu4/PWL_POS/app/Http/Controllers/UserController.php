f66cdf0b5ddbf86bf70c430e23a10bb4e536afba
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-03-12 20:08:04 +0700

    Jobsheet 4: Praktikum 2.5 (Create, Read, Update, Delete (CRUD))

--- Minggu4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu4/PWL_POS/app/Http/Controllers/UserController.php
@@ -14,19 +14,66 @@ class UserController extends Controller
          * Jobsheet 4
          */
 
-        //PRAKTIKUM 2.4- LANGKAH 10
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
+
+        //PRAKTIKUM 2.5 - LANGKAH 3
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        
+        $user->username = 'manager12';
         
         $user->save();
         
-        return view('user', ['data' => $user]);
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        
+        dd($user->wasChanged(['nama', 'username'])); // true
+
+        //PRAKTIKUM 2.5 - LANGKAH 1
+        // $user = UserModel::create([
+        //      'username' => 'manager55',
+        //      'nama' => 'Manager55',
+        //      'password' => Hash::make('12345'),
+        //      'level_id' => 2,
+        //  ]);
+         
+        //  $user->username = 'manager56';
+         
+        //  $user->isDirty(); // true
+        //  $user->isDirty('username'); // true
+        //  $user->isDirty('nama'); // false
+        //  $user->isDirty(['nama', 'username']); // true
+         
+        //  $user->isClean(); // false
+        //  $user->isClean('username'); // false
+        //  $user->isClean('nama'); // true
+        //  $user->isClean(['nama', 'username']); // false
+         
+        //  $user->save();
+         
+        //  $user->isDirty(); // false
+        //  $user->isClean(); // true
+        //  dd($user->isDirty());
+
+        //*************************************************************** */
+        //PRAKTIKUM 2.4- LANGKAH 10
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+        
+        // $user->save();
+        
+        // return view('user', ['data' => $user]);
 
         //PRAKTIKUM 2.4 - LANGKAH 8
         // $user = UserModel::firstOrNew(
