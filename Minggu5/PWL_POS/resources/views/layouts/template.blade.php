7b53418f207beaf70f4c7cd126cbde54f14536de
annisaeka123 <annisaekapuspitaaa@gmail.com>
2025-03-27 22:23:45 +0700

    Jobsheet 5: Praktikum 4 (Implementasi Filtering Datatables)

--- Minggu5/PWL_POS/resources/views/layouts/template.blade.php
+++ Minggu5/PWL_POS/resources/views/layouts/template.blade.php
@@ -12,9 +12,9 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
   <!-- DataTables -->
-  <link rel = "stylesheet" href = "{ { asset ( 'adminlte/plugins/datatables - bs4/css/dataTables.bootstrap4.min.css' ) } }" >
-  <link rel = "stylesheet" href = "{ { asset ( 'adminlte/plugins/datatables - responsive/css/responsive.bootstrap4.min.css' ) } }" >
-  <link rel = "stylesheet" href = "{ { asset ( 'adminlte/plugins/datatables - buttons/css/buttons.bootstrap4.min.css' ) } }" >
+  <link rel = "stylesheet" href = "{{ asset ('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}" >
+  <link rel = "stylesheet" href = "{{ asset ('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}" >
+  <link rel = "stylesheet" href = "{{ asset ('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}" >
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 
